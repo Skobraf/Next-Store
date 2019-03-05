815dadf3f898fe53d0d1cb4fb6cc989db816d1ef
skobraf <skobraf@gmail.com>
2019-03-05 18:06:09 +0000

    first commit imported link

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+const home = props => (
+    <div>
+        <p>hello</p>
+        <Link href="/Sell">
+        <a>Sell</a>
+        </Link>
+    </div>
+)
+
+export default home;
