3c5d959d602f05924708748ee981bcba51e53a65
skobraf <skobraf@gmail.com>
2019-03-11 14:11:42 +0000

    added first query and mutation

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,7 @@
-const Mutations = {};
+const Mutations = {
+    createDog(parent, args, ctx, info) {
+        console.log(args)
+    }
+};
 
 module.exports = Mutations;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,7 @@
-const Query = {};
+const Query = {
+    dogs(parent, args, ctx, info) {
+        return[{name: "snickers"}, {name:"bobi"}]
+    }
+};
 
 module.exports = Query;
