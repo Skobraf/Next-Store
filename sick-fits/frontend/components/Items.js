aab44bb39ebf6e3c20adea5cfa4fa2d70abfd94c
skobraf <skobraf@gmail.com>
2019-03-11 17:20:35 +0000

    added css to shop

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
+import styled from 'styled-components';
 
 const ALL_ITEMS_QUERY = gql`
     query ALL_ITEMS_QUERY {
@@ -13,12 +14,21 @@ const ALL_ITEMS_QUERY = gql`
             largeImage
         }
     }
+`;
+const Center = styled.div`
+    text-align: center;
+`;
+const ItemsList = styled.div`
+    display:grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 60px;
+    max-width: ${props => props.theme.maxWidth};
 `
 
 class Items extends Component  {
     render() {
         return (
-            <div>
+            <Center>
                 <p>
                     Items!
                 </p>
@@ -26,10 +36,12 @@ class Items extends Component  {
                     {({data, error, loading}) => {
                         if(loading) return <p>loading...</p>;
                         if(error) return <p>Error: {error.message}</p>;
-                        return <p>data are {data.items.length}</p>
+                        return <ItemsList>
+                                {data.items.map(item => <p>{item.title}</p>)}
+                            </ItemsList>
                     }}
                 </Query>
-            </div>
+            </Center>
         )
     }
 }
