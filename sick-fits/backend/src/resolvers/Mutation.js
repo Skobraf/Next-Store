ef56ee4c1cb98f3b7c4d7d3cd57b9dfe0bbdf7c9
skobraf <skobraf@gmail.com>
2019-03-11 15:12:01 +0000

    update prisma db, added item mutation

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,7 +1,16 @@
 const Mutations = {
-    createDog(parent, args, ctx, info) {
-        console.log(args)
+    async createItem(parent, args, ctx, info) {
+        const item = await ctx.db.mutation.createItem({
+            data: {
+                ...args
+            }
+        }, info);
+        console.log(item);
+        return item;
     }
+   // createDog(parent, args, ctx, info) {
+   //     console.log(args)
+   // }
 };
 
 module.exports = Mutations;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,7 +1,7 @@
 const Query = {
-    dogs(parent, args, ctx, info) {
-        return[{name: "snickers"}, {name:"bobi"}]
-    }
+   // dogs(parent, args, ctx, info) {
+    //    return[{name: "snickers"}, {name:"bobi"}]
+   // }
 };
 
 module.exports = Query;
