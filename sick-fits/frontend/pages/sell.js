9c1d0356467f88f03eb5c064446c6884e317b412
skobraf <skobraf@gmail.com>
2019-03-13 16:05:14 +0000

    fix container component

--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import CreateItem from '../components/CreatItem';
 const Sell = props => (
     <div>
-        <p>Sell!</p>
+        <CreateItem />
     </div>
 )
 export default Sell;
