81174651f9a3fb3952b08f979e3d418728bea2d9
skobraf <skobraf@gmail.com>
2019-03-14 17:05:43 +0000

    added handleChange method

--- sick-fits/frontend/components/CreatItem.js
+++ sick-fits/frontend/components/CreatItem.js
@@ -4,13 +4,58 @@ import gql from 'graphql-tag';
 import Form from './styles/Form';
 import formatMoney from '../lib/formatMoney';
 class CreatItem extends Component {
+    state={
+        title: '',
+        description: '',
+        image: '',
+        largeImage: '',
+        price:0,
+    }
+    handleChange = (e) => {
+        const { name, type, value } = e.target;
+        const val = type === 'number' ? parseFloat(value) : value;
+        this.setState({[name]: val});
+    }
     render() {
         return (
-            <div>
-                <Form>
-                    <h2>Sell an Item</h2>
-                </Form>
-            </div>
+            <Form>
+                <fieldset>
+                    <label htmlFor="title">
+                        Title
+                        <input 
+                        type="text"
+                        id="title"
+                        name="title"
+                        placeholder="Title"
+                        required
+                        value={this.state.title}
+                        onChange={this.handleChange}
+                        />
+                    </label>
+                    <label htmlFor="price">
+                        Price
+                        <input 
+                        type="number"
+                        id="price"
+                        name="price"
+                        placeholder="Price"
+                        required
+                        value={this.state.price}
+                        onChange={this.handleChange}
+                        />
+                    </label>
+                    <label htmlFor="description">
+                        Description
+                        <input 
+                        id="description"
+                        name="description"
+                        placeholder="Enter A Description"
+                        required
+                        value={this.state.description}
+                        onChange={this.handleChange}
+                        />
+                </fieldset>
+            </Form>
         );
     }
 }
