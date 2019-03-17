fa2813464dbcd642903ab4178fd2484fc7cf6757
skobraf <skobraf@gmail.com>
2019-03-17 12:05:53 +0000

    update query and mutation resolvers

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -7,6 +7,17 @@ const Mutations = {
         }, info);
         console.log(item);
         return item;
+    },
+    updateItem(parent, args, ctx, info) {
+        //take a copy of the updates
+        const updates = { ...args };
+        // remove the ID from updates
+        delete updates.id;
+        // run the update method
+        return ctx.db.mutation.updateItem({
+            data: updates,
+            where: {id: args.id}
+        },info )
     }
 };
 
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,8 +1,13 @@
+const { forwardTo } = require('prisma-binding');
+
 const Query = {
-   async items(parent, args, ctx, info) {
-      const items = await ctx.db.query.items()
-      return items;
-   }
+   items: forwardTo('db'),
+   item: forwardTo('db'),
+
+   // async items(parent, args, ctx, info) {
+   //    const items = await ctx.db.query.items()
+   //    return items;
+   // }
 };
 
 module.exports = Query;
