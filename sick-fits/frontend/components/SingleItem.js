f8b510cc45984685edb0c99c14844f439c79d7c7
skobraf <skobraf@gmail.com>
2019-03-18 15:02:22 +0000

    added item page and component to display single item

---
+++ sick-fits/frontend/components/SingleItem.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+import Error from './ErrorMessage';
+
+const SINGLE_ITEM_QUERY = gql`
+    query SINGLE_ITEM_QUERY($id: ID!) {
+        item(where: {id: $id }) {
+            id 
+            title
+            description
+            largeImage
+        }
+    }
+    `;
+class SingleItem extends Component {
+    render() {
+        return (
+            <Query 
+                query={SINGLE_ITEM_QUERY}
+                variables={{id: this.props.id}}
+            >
+                {({ error, loading, data }) => {
+                    if(error) return <Error error={error} />
+                    if (loading) return <p>Loading...</p>
+                    if(!data.item) return <p>Not item found for {this.props.id}</p>
+                    console.log(data);
+                    return <p>single item component {this.props.id}</p>
+                }}
+            </Query>
+        );
+    }
+}
+
+export default SingleItem;
