a44c2a3cf9cbc56fdac856b3133459042abe0970
skobraf <skobraf@gmail.com>
2019-03-20 17:08:09 +0000

    added the sign in schema and resolver mutation

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -54,6 +54,26 @@ const Mutations = {
         });
         // return the user to the browser
         return user
+    },
+    async signin(parent, {email, password}, ctx, info) {
+        // check if there is a user with that email 
+        const user = await ctx.db.query.user({where: email});
+        if(!user) {
+            throw new Error('No such user found for the email');
+        }
+        // check if the password is correct
+        const valid = await bcrypt.compare(password, user.password);
+        if(!valid) {
+            throw new Error('Invalid password');
+        }
+        //genere the JWT token 
+        const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+        // set the cookire with the token 
+        ctx.response.cookie('token', token, {
+            httpOnly:true,
+            maxAge: 1000 * 60 * 60 * 24 * 365,
+        });
+        return user;
     }
 };
 
