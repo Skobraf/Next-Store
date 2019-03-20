eb6e21bbcf261ddac29d38e7407eaaa8768c8a10
skobraf <skobraf@gmail.com>
2019-03-20 17:57:00 +0000

    showing the user after signup

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -55,26 +55,27 @@ const Mutations = {
         // return the user to the browser
         return user
     },
-    async signin(parent, {email, password}, ctx, info) {
-        // check if there is a user with that email 
-        const user = await ctx.db.query.user({where: email});
-        if(!user) {
-            throw new Error('No such user found for the email');
+    async signin(parent, { email, password }, ctx, info) {
+        // 1. check if there is a user with that email
+        const user = await ctx.db.query.user({ where: { email } });
+        if (!user) {
+          throw new Error(`No such user found for email ${email}`);
         }
-        // check if the password is correct
+        // 2. Check if their password is correct
         const valid = await bcrypt.compare(password, user.password);
-        if(!valid) {
-            throw new Error('Invalid password');
+        if (!valid) {
+          throw new Error('Invalid Password!');
         }
-        //genere the JWT token 
+        // 3. generate the JWT Token
         const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
-        // set the cookire with the token 
+        // 4. Set the cookie with the token
         ctx.response.cookie('token', token, {
-            httpOnly:true,
-            maxAge: 1000 * 60 * 60 * 24 * 365,
+          httpOnly: true,
+          maxAge: 1000 * 60 * 60 * 24 * 365,
         });
+        // 5. Return the user
         return user;
-    }
+      },
 };
 
 module.exports = Mutations;
