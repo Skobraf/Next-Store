7f1b180405ea58bb3e2d745b230cbdffbe938b91
skobraf <skobraf@gmail.com>
2019-03-20 16:03:20 +0000

    fixed pagination issue from query resolvers

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -5,14 +5,17 @@ const Query = {
    item: forwardTo('db'),
    itemsConnection: forwardTo('db'),
    me(parent, args, ctx, info) {
-      if(!ctx.request.userId) {
-         return null;
-      }
-      return ctx.db.query.user({
-         where: { id: ctx.request.userId }
-      }, info);
-   }
- }
-};
+     // check if there is a current user ID
+     if (!ctx.request.userId) {
+       return null;
+     }
+     return ctx.db.query.user(
+       {
+         where: { id: ctx.request.userId },
+       },
+       info
+     );
+   },
+ };
 
 module.exports = Query;
--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,7 +1,15 @@
 import Link from 'next/link';
 import NavStyles from './styles/NavStyles';
+import User from './User';
+
 const Nav = () => (
     <NavStyles>
+        <User>
+            {data => {
+                console.log(data);
+                return <p>User</p>
+            }}
+        </User>
         <Link href="/items">
         <a>Shop</a>
         </Link>
