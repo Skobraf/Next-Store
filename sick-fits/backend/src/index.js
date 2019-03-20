11a35ff3c0728c288f4e3b38d04e2d3ebdaf5899
skobraf <skobraf@gmail.com>
2019-03-20 09:27:50 +0000

    added cookiParser to express config

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,3 +1,4 @@
+const cookieParser = require('cookie-parser');
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
@@ -5,6 +6,7 @@ const db = require('./db');
 const server = createServer();
 
 // TODO Use express middlware to handle cookies (JWT)
+server.express.use(cookieParser())
 // TODO Use express middlware to populate current user
 
 server.start(
