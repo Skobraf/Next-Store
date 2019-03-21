d69018c332902055bff3dde6e550d8c81633218f
skobraf <skobraf@gmail.com>
2019-03-21 15:43:20 +0000

    added signout mutation in resolvers

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -76,6 +76,10 @@ const Mutations = {
         // 5. Return the user
         return user;
       },
+      signout(parent, args, ctx, info) {
+          ctx.response.clearCookie('token');
+          return { message: 'GoodBye!' };
+      }
 };
 
 module.exports = Mutations;
