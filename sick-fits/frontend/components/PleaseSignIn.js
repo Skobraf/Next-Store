7e5157970939706e317b57e67dd72c6f02907903
skobraf <skobraf@gmail.com>
2019-03-24 13:59:37 +0000

    fiix pleaseSignin component

--- sick-fits/frontend/components/PleaseSignIn.js
+++ sick-fits/frontend/components/PleaseSignIn.js
@@ -1,10 +1,10 @@
 import { Query } from 'react-apollo';
 import { CURRENT_USER_QUERY } from './User';
-import { Signin } from './Signin';
+import Signin from './Signin';
 
 const PleaseSignIn = props => (
     <Query query={CURRENT_USER_QUERY}>
-        {(data, loading) => {
+        {({ data, loading }) => {
             if(loading) return <p>Loading...</p>;
             if(!data.me) {
                 return (
