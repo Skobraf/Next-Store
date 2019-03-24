b076db455f7b9b1791a4d84d0124b217e3c2f3ee
skobraf <skobraf@gmail.com>
2019-03-24 13:33:11 +0000

    establish data relation between user and item

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -3,12 +3,21 @@ const jwt = require('jsonwebtoken');
 const { randomBytes } = require('crypto');
 const { promisify } = require('util');
 
-const { transport, maeANiceEmail } = require('util');
+const { transport, maeANiceEmail } = require('../mail.js');
 
 const Mutations = {
     async createItem(parent, args, ctx, info) {
+        if(!ctx.request.userId) {
+            throw new Error('You must be logged in!')
+        }
         const item = await ctx.db.mutation.createItem({
             data: {
+                // This is how to create a relationship between the Item and the User in Prisma
+                user: {
+                    connect: {
+                        id: ctx.request.userId
+                    }
+                },
                 ...args
             }
         }, info);
