453e4acb03180f99482eabe4d473868a55b683eb
skobraf <skobraf@gmail.com>
2019-03-24 13:54:33 +0000

    added checking if the user is not sign in

--- sick-fits/frontend/components/PleaseSignIn.js
+++ sick-fits/frontend/components/PleaseSignIn.js
@@ -2,6 +2,20 @@ import { Query } from 'react-apollo';
 import { CURRENT_USER_QUERY } from './User';
 import { Signin } from './Signin';
 
-const PleaseSignIn = props => <p>Please Sign in!</p>
+const PleaseSignIn = props => (
+    <Query query={CURRENT_USER_QUERY}>
+        {(data, loading) => {
+            if(loading) return <p>Loading...</p>;
+            if(!data.me) {
+                return (
+                    <div>
+                        <p>Please sign in to Continue</p>
+                        <Signin />
+                    </div>
+                )
+            }
+        }}
+    </Query>
+)
 
 export default PleaseSignIn;
