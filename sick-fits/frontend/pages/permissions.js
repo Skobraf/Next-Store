911003d71a8068671a62675578580da7cea90767
skobraf <skobraf@gmail.com>
2019-03-24 14:19:11 +0000

    initiate permission page

---
+++ sick-fits/frontend/pages/permissions.js
@@ -0,0 +1,10 @@
+
+import PleaseSignIn from '../components/PleaseSignIn';
+const Permissions = props => (
+    <div>
+        <PleaseSignIn>
+          <p>Permissions</p>
+        </PleaseSignIn>
+    </div>
+)
+export default Permissions;
